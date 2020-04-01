898bd3e9b45990023c769a971e245f215d9dca60
N0SBa <nguyenbason.fpt04041999@gmail.com>
2020-04-01 22:46:55 +0700

    Update module Admin + gen model

--- FinalProject/FinalProject/Controllers/Admin/AccountManagementController.cs
+++ FinalProject/FinalProject/Controllers/Admin/AccountManagementController.cs
@@ -1,4 +1,6 @@
 ﻿using FinalProject.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -15,6 +17,12 @@ namespace FinalProject.Controllers
         [HttpGet]
         public ActionResult JobSeekerManagement()
         {
+            //get list Role
+            using (FinalProjectEntities db = new FinalProjectEntities())
+            {
+                var listRole = db.User_roles.ToList();
+                ViewBag.listRole = listRole;
+            }
             return View("~/Views/Admin/JobSeeker/ListJobSeeker.cshtml");
         }
 
@@ -30,11 +38,12 @@ namespace FinalProject.Controllers
                 string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
                 string sortDirection = Request["order[0][dir]"];
 
-                var mySql = @"select 
+                var mySql = @"select
+                            u.ID,
                             u.Username,
-                            u.Password, 
+                            u.Name, 
                             u.Email, 
-                            us.Name 
+                            us.Name as 'RoleName'
                             from [User] u join User_roles us on u.RoleID = us.ID 
                             order by " + sortColumnName + " " + sortDirection + " OFFSET " + start + " ROWS FETCH NEXT " + length + " ROWS ONLY";
                 using (FinalProjectEntities db = new FinalProjectEntities())
@@ -63,17 +72,13 @@ namespace FinalProject.Controllers
 
                 using (FinalProjectEntities db = new FinalProjectEntities())
                 {
-                    //check dupicate username
-                    //var duplicateSql = @"select * from [User]
-                    //                    where Username = @username";
-                    //var duplicateUser = db.Database.SqlQuery<User>(duplicateSql,
-                    //    new SqlParameter("@username", username)).ToList();
                     var duplicateUser = db.Users.Where(s => s.Username == username).FirstOrDefault();
                     if (duplicateUser == null)
                     {
                         User us = new User();
                         us.Username = username;
                         us.Password = password;
+                        us.RoleID = 2;
                         db.Users.Add(us);
                         db.SaveChanges();
                         return Json(new { success = true, title = "Thành công", message = "Thêm tài khoản thành công."});
@@ -88,12 +93,96 @@ namespace FinalProject.Controllers
             }
         }
 
+        ///Get Account By ID
+        [HttpPost]
+        public ActionResult getAccountByID()
+        {
+            try
+            {
+                var user_id = Request["user_id"];
+                using (FinalProjectEntities db = new FinalProjectEntities())
+                {
+                    var mySql = @"select
+                            u.*, 
+                            us.Name as 'RoleName'
+                            from [User] u join User_roles us on u.RoleID = us.ID 
+							where u.ID = @user_id";
+                    var accountByID = db.Database.SqlQuery<Account>(mySql, new SqlParameter("@user_id", user_id)).FirstOrDefault();
+                    if (accountByID != null)
+                    {
+                        return Json(new { success = true, accountByID = accountByID });
+                    } else
+                    {
+                        return Json(new { success = false, title = "Lỗi", message = "Lấy dữ liệu tài khoản gặp lỗi." });
+                    }
+                }
+
+            } catch (Exception e)
+            {
+                return Json(new { error = true, message = "Có lỗi xảy ra." });
+            }
+        }
+
+        ///UPDATE
+        [HttpPost]
+        public ActionResult EditAccount()
+        {
+            try
+            {
+                var account = Request["account"];
+                var j_account = JObject.Parse(account);
+                using (FinalProjectEntities db = new FinalProjectEntities())
+                {
+                    var u_account = db.Users.Find(Convert.ToInt32(j_account.GetValue("id")));
+                    if (u_account != null)
+                    {
+                        u_account.Password = j_account.GetValue("password").ToString();
+                        u_account.Name = j_account.GetValue("name").ToString();
+                        u_account.Email = j_account.GetValue("email").ToString();
+                        u_account.CV_Link = j_account.GetValue("cv_link").ToString();
+                        u_account.RoleID = Convert.ToInt32(j_account.GetValue("role_id").ToString());
+                        db.SaveChanges();
+                        return Json(new { success = true, title = "Thành công", message = "Cập nhật tài khoản thành công." });
+                    } else
+                    {
+                        return Json(new { success = false, title = "Lỗi", message = "Cập nhật tài khoản lỗi." });
+                    }
+                }
+            } catch (Exception e)
+            {
+                return Json(new { error = true, message = "Có lỗi xảy ra." });
+            }
+        }
+
+        ///DELETE
+        [HttpPost]
+        public ActionResult DeleteAccount()
+        {
+            try
+            {
+                var user_id = Request["user_id"];
+                using (FinalProjectEntities db = new FinalProjectEntities())
+                {
+                    var account = db.Users.Find(Convert.ToInt32(user_id));
+                    if (account != null)
+                    {
+                        db.Users.Remove(account);
+                        db.SaveChanges();
+                        return Json(new { success = true, title = "Thành công", message = "Xóa tài khoản thành công." });
+                    } else
+                    {
+                        return Json(new { success = true, title = "Lỗi", message = "Không tìm thấy tài khoản cần xóa." });
+                    }
+                }
+            } catch (Exception e)
+            {
+                return Json(new { error = true, message = "Có lỗi xảy ra." });
+            }
+        }
+
         ///Custom Entites
-        public class Account {
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public string Email { get; set; }
-            public string Name { get; set; }
+        public class Account : User {
+            public string RoleName { get; set; }
         }
     }
 }
