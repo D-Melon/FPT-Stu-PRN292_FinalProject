d53c93f91bfad5e4634d8ce95ee93db89c3f3cc3
toantd2001 <49900584+toantd2001@users.noreply.github.com>
2020-04-02 11:51:55 +0700

    Update Jobseeker home page

--- FinalProject/FinalProject/Controllers/JobseekerController.cs
+++ FinalProject/FinalProject/Controllers/JobseekerController.cs
@@ -14,12 +14,27 @@ namespace FinalProject.Controllers
         {
             FinalProjectEntities db = new FinalProjectEntities();
             ViewData["Notification"] = Session["Notification"] as string;
-            List<Company_Full> companies = db.Database.SqlQuery<Company_Full>(@"SELECT Top 10  Employer.ID, Employer.Company_Name, Employer.Company_Web, Employer.Name, Employer.Title, Employer.Email, Employer.Phone, Location.Name AS LocationName
+            String cur_page = Request["page"];
+            if (cur_page == null) cur_page = "1";
+
+            int cur = Convert.ToInt32(cur_page);
+            int company_in_pages = 5;
+            int total_company = db.Employers.Count();
+
+            int total_pages = total_company % company_in_pages == 0 ? total_company / company_in_pages : total_company / company_in_pages + 1;
+
+            int from = cur * company_in_pages - (company_in_pages - 1);
+            int to = from + company_in_pages - 1;
+            List<Company_Full> companies = db.Database.SqlQuery<Company_Full>(@"select * from (
+                    	                                                        SELECT ROW_NUMBER() over (order by Employer.ID) as number ,Employer.ID, Employer.Company_Name, Employer.Company_Web, Employer.Name, Employer.Title, Employer.Email, Employer.Phone, Location.Name AS LocationName
                                                                                 FROM Employer INNER JOIN
-                                                                                Location ON Employer.ID_Location = Location.ID").ToList();
-            User u = (User)Session["account"];
-            ViewBag.user = u;
+                                                                                Location ON Employer.ID_Location = Location.ID)
+                    	                                                        as Data where Number between "+from+" and "+to).ToList();
+            User u = Session["account"] as User;
+            ViewBag.name = u.Name;
             ViewBag.companies = companies;
+            ViewBag.cur = cur;
+            ViewBag.pages = total_pages;
             return View();
         }
 
